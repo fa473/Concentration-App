46f1fc5c870247a70ae67f1a27cf512b4f4dc169
fa473 <fa473@users.noreply.github.com>
2017-11-21 18:10:01 -0800

    implement autolayout

--- Concentration/ViewController.swift
+++ Concentration/ViewController.swift
@@ -10,9 +10,13 @@ import UIKit
 
 class ViewController: UIViewController{
     
-    lazy var game = Concentration(numberOfPairsOfCards: (cardButtons.count + 1) / 2)
+    lazy var game = Concentration(numberOfPairsOfCards: numberOfPairsOfCards)
     
-    var flipCount = 0{
+    var numberOfPairsOfCards: Int {
+        return ((cardButtons.count + 1) / 2)
+    }
+    
+    var flipCount = 0 {
         didSet {
             flipCountLabel.text = "Flips: \(flipCount)"
         }
